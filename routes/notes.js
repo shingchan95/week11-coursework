5d4d5ba5f4f11529abf189aa96c1e0329ac82291
shingchan95 <chanhei888@gmail.com>
2022-05-27 16:06:37 +0100

    update

---
+++ routes/notes.js
@@ -0,0 +1,40 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+
+
+notes.get('/', (req,res) => {
+    console.info(`${req.method} request received for feedback`);
+    
+    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to submit feedback`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title,text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newText = {
+        title,
+        text,
+      };
+  
+      readAndAppend(newText, '../db/db.json');
+  
+      const response = {
+        status: 'success',
+        body: newText,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting text');
+    }
+  });
+  
+  module.exports = notes;
